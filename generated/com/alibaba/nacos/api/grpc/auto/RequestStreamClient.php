3316c7a1383bbd0500dbba88b563e2e9f31450b8
luyanbo <robert.lyb@alibaba-inc.com>
2021-07-13 17:49:23 +0800

    use nacos 2 protocol

---
+++ generated/com/alibaba/nacos/api/grpc/auto/RequestStreamClient.php
@@ -0,0 +1,50 @@
+<?php
+// GENERATED CODE -- DO NOT EDIT!
+
+// Original file comments:
+//
+// Copyright 1999-2020 Alibaba Group Holding Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+namespace com\alibaba\nacos\api\grpc\auto;
+
+/**
+ */
+class RequestStreamClient extends \Grpc\BaseStub {
+
+    /**
+     * @param string $hostname hostname
+     * @param array $opts channel options
+     * @param \Grpc\Channel $channel (optional) re-use channel object
+     */
+    public function __construct($hostname, $opts, $channel = null) {
+        parent::__construct($hostname, $opts, $channel);
+    }
+
+    /**
+     * build a streamRequest
+     * @param \com\alibaba\nacos\api\grpc\auto\Payload $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\ServerStreamingCall
+     */
+    public function requestStream(\com\alibaba\nacos\api\grpc\auto\Payload $argument,
+      $metadata = [], $options = []) {
+        return $this->_serverStreamRequest('/RequestStream/requestStream',
+        $argument,
+        ['\com\alibaba\nacos\api\grpc\auto\Payload', 'decode'],
+        $metadata, $options);
+    }
+
+}
